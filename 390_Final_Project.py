448705d846f198efd0b7ab08f1ba9945c3a7efdf
Abigail O'Meara <aomeara@smith.edu>
2023-12-05 10:21:24 -0500

    edits to file

--- 390_Final_Project.py
+++ 390_Final_Project.py
@@ -1,8 +1,18 @@
 # import packages
-import pandas # import packages
 import pandas as pd
 
 # Load and view raw data
 url = 'https://raw.githubusercontent.com/abbeyo55/sds_390_final/main/REMS_Mars_Dataset.csv'
 mars = pd.read_csv(url)
+mars
+
+
+
+# Data cleaning
+
+# Create properly formatted datetime index
+mars['earth_date_time'] = mars['earth_date_time'].astype(str)
+mars['date'] = mars['earth_date_time'].str.replace('Earth, ', '').str.replace(' UTC', '') # remove unnecessary info to get just the date
+mars['date'] = pd.to_datetime(mars['date'], format='%Y-%m-%d %H:%M:%S', errors='coerce')
+
 mars
